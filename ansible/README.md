5e19418b488b77afe9b6bcd4242576b270f8e66c
Ivo Jimenez <ivo.jimenez@gmail.com>
2015-10-31 00:56:14 -0700

    Adds ansible files to execute an end-to-end experiment

---
+++ ansible/README.md
@@ -0,0 +1,26 @@
+# playbook for porta
+
+First, edit inventory to point to `base` and `targets`. Secondly, if 
+ansible is already installed:
+
+```bash
+ansible-galaxy install angstwad/docker_ubuntu
+ansible-playbook \
+  -i inventory \
+  -b \
+  --extra-vars \
+    '{ "benchmarks": ["ivotron/comd","ivotron/redisbench"]}'
+  playbook.yml
+```
+
+In docker:
+
+```
+docker build ansible-port .
+docker run ansible-port
+```
+
+Dependencies:
+
+  * docker 1.7+ on localhost
+  * Ubuntu 12.04+ on remote nodes
---
+++ ansible/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# Simple wrapper for executing ansible-galaxy and ansible-playbook
+# with local connection.
+#
+# USAGE:
+#    ansible-playbook-wrapper  [other ansible-playbook arguments]
+#
+# ENVIRONMENT VARIABLES:
+#
+#    - REQUIREMENTS: requirements filename; default = "requirements.yml"
+#    - PLAYBOOK:     playbook filename;     default = "playbook.yml"
+#    - INVENTORY:    inventory filename;    default = "/etc/ansible/hosts"
+#
+
+cd /app
+
+INVENTORY=inventory
+
+# install Galaxy roles, if any
+if [ -z "$REQUIREMENTS" ]; then
+    REQUIREMENTS=requirements.yml
+fi
+
+if [ -f "$REQUIREMENTS" ]; then
+    ansible-galaxy install -p roles/ -r $REQUIREMENTS
+fi
+
+# execute playbook
+if [ -z "$PLAYBOOK" ]; then
+    PLAYBOOK=playbook.yml
+fi
+
+exec ansible-playbook -i $INVENTORY $PLAYBOOK "$@"
