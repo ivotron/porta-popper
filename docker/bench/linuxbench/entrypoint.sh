0cb4ccabcc5490d684c73fc1d07048b0a87db6f8
Ivo Jimenez <ivo.jimenez@gmail.com>
2015-12-03 14:44:54 -0800

    Updates linuxbench to include more benchmarks

--- docker/bench/linuxbench/entrypoint.sh
+++ docker/bench/linuxbench/entrypoint.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 
-phoronix-test-suite batch-benchmark pts/linux-system &> /tmp/out
+phoronix-test-suite batch-benchmark \
+       pts/bullet \
+       pts/byte \
+       pts/cachebench \
+       pts/compress-lzma \
+       pts/compress-gzip \
+       pts/compress-pbzip2 \
+       pts/compress-7zip \
+       pts/dcraw \
+       pts/encode-mp3 \
+       pts/encode-flac \
+       pts/encode-ogg \
+       pts/encode-ape \
+       pts/encode-wavpack \
+       pts/etqw-demo \
+       pts/ffmpeg \
+       pts/fhourstones \
+       pts/gcrypt \
+       pts/gnupg \
+       pts/gmpbench \
+       pts/graphics-magick \
+       pts/himeno \
+       pts/hmmer \
+       pts/john-the-ripper \
+       pts/jxrendermark \
+       pts/lightsmark \
+       pts/mafft \
+       pts/mencoder \
+       pts/minion \
+       pts/mrbayes \
+       pts/n-queens \
+       pts/nero2d \
+       pts/npb \
+       pts/openssl \
+       pts/padman \
+       pts/povray \
+       pts/pybench \
+       pts/scimark2 \
+       pts/smallpt \
+       pts/sunflow \
+       pts/sudokut \
+       pts/tachyon \
+       pts/tscp \
+       pts/ttsiod-renderer \
+       pts/vdrift \
+       pts/x264 &> /tmp/out
 if [ $? -ne 0 ] ; then
   echo "ERROR while executing benchmark"
   cat /tmp/out
