70cd43897d348051d76a86165c3d59745c216845
Ivo Jimenez <ivo.jimenez@gmail.com>
2015-12-11 00:36:23 -0800

    Mods to benchmarks so they can be profiled

--- docker/bench/sysbench/entrypoint.sh
+++ docker/bench/sysbench/entrypoint.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
-sysbench --test=cpu --cpu-max-prime=20000 run > output
-
-if [ $? -ne 0 ] ; then
-   cat output
-   exit 1
+if [ -z $BENCHMARKS ] ; then
+  BENCHMARKS="cpu mem"
 fi
 
-result=`cat output | grep 'total time:' | awk '{ print $NF }' | sed 's/s//'`
-echo "[{"
-echo "\"name\": \"sysbench-cpu\", "
-echo "\"class\": \"processor\", "
-echo "\"result\": \"$result\" "
-echo "},"
+echo "["
+if [[ $BENCHMARKS == *"cpu"* ]] ; then
+  sysbench --test=cpu --cpu-max-prime=20000 run > output
 
-sysbench --test=memory run > output
+  if [ $? -ne 0 ] ; then
+     cat output
+     exit 1
+  fi
 
-if [ $? -ne 0 ] ; then
-   cat output
-   exit 1
+  result=`cat output | grep 'total time:' | awk '{ print $NF }' | sed 's/s//'`
+  echo "{"
+  echo "\"name\": \"sysbench-cpu\", "
+  echo "\"class\": \"processor\", "
+  echo "\"result\": \"$result\" "
+  echo "}"
 fi
 
-result=`cat output | grep 'total time:' | awk '{ print $NF }' | sed 's/s//'`
-echo "{"
-echo "\"name\": \"sysbench-memory\", "
-echo "\"class\": \"memory\", "
-echo "\"result\": \"$result\" "
-echo "}]"
+if [[ $BENCHMARKS == *"mem"* ]] && [[  $BENCHMARKS == *"cpu"* ]] ; then
+  echo ","
+fi
+
+if [[ $BENCHMARKS == *"mem"* ]] ; then
+  sysbench --test=memory run > output
+
+  if [ $? -ne 0 ] ; then
+     cat output
+     exit 1
+  fi
+
+  result=`cat output | grep 'total time:' | awk '{ print $NF }' | sed 's/s//'`
+  echo "{"
+  echo "\"name\": \"sysbench-memory\", "
+  echo "\"class\": \"memory\", "
+  echo "\"result\": \"$result\" "
+  echo "}"
+fi
+
+echo "]"
