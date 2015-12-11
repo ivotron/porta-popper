fca7cb4bce793d82e166bba2d9923c89099f6ba5
Ivo Jimenez <ivo.jimenez@gmail.com>
2015-12-10 19:23:25 -0800

    Parametrizes unixbench tests
    via the BENCHMARKS environment variable, benchmarks from unixbench can
    be selected

--- docker/bench/unixbench/entrypoint.sh
+++ docker/bench/unixbench/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-/app/UnixBench/Run -c 1 dhry2reg syscall pipe context1 spawn execl shell1 &> /tmp/out
+if [ -z "$BENCHMARKS" ] ; then
+  BENCHMARKS="dhry2reg syscall pipe context1 spawn execl shell1"
+fi
+
+/app/UnixBench/Run -c 1 $BENCHMARKS &> /tmp/out
 
 if [ $? -ne 0 ] ; then
   cat /tmp/out
@@ -9,27 +13,40 @@ fi
 rm results/*.log results/*.html
 
 echo "["
-result=`cat results/* | sed -n 's/Dhrystone 2 using register variables *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
-echo "{\"name\": \"unixbench-dhrystone\",\"class\": \"processor\",\"result\": $result},"
 
-result=`cat results/* | sed -n 's/System Call Overhead *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
-echo "{\"name\": \"unixbench-syscall\",\"class\": \"processor\",\"result\": $result},"
+if [[ $BENCHMARKS == *"dhry2reg"* ]] ; then
+  result=`cat results/* | sed -n 's/Dhrystone 2 using register variables *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
+  echo "{\"name\": \"unixbench-dhrystone\",\"class\": \"processor\",\"result\": $result},"
+fi
 
-result=`cat results/* | sed -n 's/Pipe-based Context Switching *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
-echo "{\"name\": \"unixbench-context\",\"class\": \"processor\",\"result\": $result},"
+if [[ $BENCHMARKS == *"syscall"* ]] ; then
+  result=`cat results/* | sed -n 's/System Call Overhead *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
+  echo "{\"name\": \"unixbench-syscall\",\"class\": \"processor\",\"result\": $result},"
+fi
 
-result=`cat results/* | sed -n 's/Process Creation *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
-echo "{\"name\": \"unixbench-spawn\",\"class\": \"processor\",\"result\": $result},"
+if [[ $BENCHMARKS == *"context1"* ]] ; then
+  result=`cat results/* | sed -n 's/Pipe-based Context Switching *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
+  echo "{\"name\": \"unixbench-context\",\"class\": \"processor\",\"result\": $result},"
+fi
 
-result=`cat results/* | sed -n 's/Execl Throughput *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
-echo "{\"name\": \"unixbench-execl\",\"class\": \"processor\",\"result\": $result},"
+if [[ $BENCHMARKS == *"spawn"* ]] ; then
+  result=`cat results/* | sed -n 's/Process Creation *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
+  echo "{\"name\": \"unixbench-spawn\",\"class\": \"processor\",\"result\": $result},"
+fi
 
-result=`cat results/* | sed -n 's/Shell Scripts (1 concurrent) *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
-echo "{\"name\": \"unixbench-shell\",\"class\": \"processor\",\"result\": $result},"
+if [[ $BENCHMARKS == *"execl"* ]] ; then
+  result=`cat results/* | sed -n 's/Execl Throughput *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
+  echo "{\"name\": \"unixbench-execl\",\"class\": \"processor\",\"result\": $result},"
+fi
 
-result=`cat results/* | sed -n 's/Pipe Throughput *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
-echo "{\"name\": \"unixbench-pipe\",\"class\": \"processor\",\"result\": $result}"
+if [[ $BENCHMARKS == *"shell1"* ]] ; then
+  result=`cat results/* | sed -n 's/Shell Scripts (1 concurrent) *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
+  echo "{\"name\": \"unixbench-shell\",\"class\": \"processor\",\"result\": $result},"
+fi
+
+if [[ $BENCHMARKS == *"pipe"* ]] ; then
+  result=`cat results/* | sed -n 's/Pipe Throughput *\(.*\) .* samples)/\1/p' | awk '{ print $1 }'`
+  echo "{\"name\": \"unixbench-pipe\",\"class\": \"processor\",\"result\": $result}"
+fi
 
 echo "]"
-
-result=`cat results/* | grep "Pipe Throughput" | awk '{ print $3 }'`
