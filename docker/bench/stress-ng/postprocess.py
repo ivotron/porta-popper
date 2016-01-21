113f8103856f58c636a229c0f932216779df9f27
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-01-21 14:47:15 -0800

    Reports 'real-time' for stress-ng

--- docker/bench/stress-ng/postprocess.py
+++ docker/bench/stress-ng/postprocess.py
@@ -10,17 +10,17 @@ if len(sys.argv) == 2:
     stressor_name = sys.argv[1]
     metric = y['metrics'][0]
     print("{")
-    print("\"name\": stressng-"+stressor_name+",")
-    print("\"class\": cpu,")
-    print("\"units\": bogo-ops-per-second,")
-    print("\"result\": "+str(metric['bogo-ops-per-second-usr-sys-time']))
+    print("\"name\": \"stressng-"+stressor_name+"\",")
+    print("\"class\": \"cpu\",")
+    print("\"units\": \"bogo-ops-per-second\",")
+    print("\"result\": "+str(metric['bogo-ops-per-second-real-time']))
     print("}")
 else:
     i = 0
     for metrics in y['metrics']:
         print("{")
-        print("\"name\": stressng-" + metrics['stressor'] + ",")
-        print("\"result\": " + str(metrics['bogo-ops-per-second-usr-sys-time']))
+        print("\"name\": \"stressng-" + metrics['stressor'] + "\",")
+        print("\"result\": " + str(metrics['bogo-ops-per-second-real-time']))
         if i < len(y['metrics'])-1:
             print("},")
         else:
