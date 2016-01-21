2a18ee47a0b0946e680eef52c8f137adf9302c60
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-01-21 10:47:54 -0800

    Makes cpu-methods the default benchmark for stress-ng

--- docker/bench/stress-ng/entrypoint.sh
+++ docker/bench/stress-ng/entrypoint.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 if [ -z $BENCHMARKS ] ; then
-  BENCHMARKS="cpu"
+  BENCHMARKS="cpu-methods"
+fi
+
+if [ -z $NUM_WORKERS ] ; then
+  NUM_WORKERS=1
 fi
 
 if [ -z $TIMEOUT ] ; then
-  TIMEOUT=60
+  TIMEOUT=10
 fi
 
 if [[ $BENCHMARKS == *"cpu-methods"* ]] ; then
-  for method in "ackermann bitops callfunc crc16 cdouble cfloat clongdouble correlate decimal32 decimal64 decimal128 double djb2a euler explog fibonacci fnv1a fft float gamma gcd gray hamming hanoi hyperbolic idct int128 int64 int32 int16 int8 int128float int128double int128longdouble int128decimal32 int128decimal64 int128decimal128 int64float int64double int64longdouble int32float int32double int32longdouble jenkin jmp ln2 longdouble loop matrixprod nsqrt omega phi pi pjw prime psi rand rgb sdbm sieve sqrt trig zeta"; do
-     stress-ng --cpu 1 --cpu-method $method -t $TIMEOUT --metrics-brief --times -Y /out.yaml &> /dev/null
+  echo "["
+  for method in ackermann bitops callfunc crc16 cdouble cfloat clongdouble correlate decimal32 decimal64 decimal128 double djb2a euler explog fibonacci fnv1a fft float gamma gcd gray hamming hanoi hyperbolic idct int128 int64 int32 int16 int8 int128float int128double int128longdouble int128decimal32 int128decimal64 int128decimal128 int64float int64double int64longdouble int32float int32double int32longdouble jenkin jmp ln2 longdouble loop matrixprod nsqrt omega phi pi pjw prime psi rand rgb sdbm sieve sqrt trig zeta ; do
+     stress-ng --cpu $NUM_WORKERS --cpu-method $method -t $TIMEOUT --metrics-brief --times -Y /out.yaml &> /dev/null
+     /postprocess.py $method
+     if [ "$method" != zeta ] ; then
+       echo ","
+     fi
   done
-elif [[ $BENCHMARKS == *"cpu"* ]] ; then
-  stress-ng --class cpu --sequential 1 -t $TIMEOUT --metrics-brief -Y /out.yaml &> /dev/null
+  echo "]"
+  exit 0
+fi
+
+echo "["
+if [[ $BENCHMARKS == *"cpu"* ]] ; then
+  stress-ng --class cpu --sequential $NUM_WORKERS -t $TIMEOUT --metrics-brief -Y /out.yaml &> /dev/null
 elif [[ $BENCHMARKS == *"mem"* ]] ; then
-  stress-ng --class memory --sequential 1 -t $TIMEOUT --metrics-brief -Y /out.yaml &> /dev/null
+  stress-ng --class memory --sequential $NUM_WORKERS -t $TIMEOUT --metrics-brief -Y /out.yaml &> /dev/null
 elif [[ $BENCHMARKS == *"cache"* ]] ; then
-  stress-ng --class cpu-cache --sequential 1 -t $TIMEOUT --metrics-brief -Y /out.yaml &> /dev/null
+  stress-ng --class cpu-cache --sequential $NUM_WORKERS -t $TIMEOUT --metrics-brief -Y /out.yaml &> /dev/null
 else
   echo "Unknown value for BENCHMARKS: $BENCHMARKS"
   exit 1
 fi
+echo "]"
 
 /postprocess.py
--- docker/bench/stress-ng/postprocess.py
+++ docker/bench/stress-ng/postprocess.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 
 import yaml
+import sys
 
 with open('/out.yaml', 'r') as f:
     y = yaml.load(f)
 
-print("[")
-i = 0
-for metrics in y['metrics']:
+if len(sys.argv) == 2:
+    stressor_name = sys.argv[1]
+    metric = y['metrics'][0]
     print("{")
-    print("\"name\": " + metrics['stressor'] + ",")
-    print("\"result\": " + str(metrics['bogo-ops-per-second-usr-sys-time']))
-    if i < len(y['metrics'])-1:
-        print("},")
-    else:
-        print("}")
-    i += 1
-print("]")
+    print("\"name\": stressng-"+stressor_name+",")
+    print("\"class\": cpu,")
+    print("\"units\": bogo-ops-per-second,")
+    print("\"result\": "+str(metric['bogo-ops-per-second-usr-sys-time']))
+    print("}")
+else:
+    i = 0
+    for metrics in y['metrics']:
+        print("{")
+        print("\"name\": stressng-" + metrics['stressor'] + ",")
+        print("\"result\": " + str(metrics['bogo-ops-per-second-usr-sys-time']))
+        if i < len(y['metrics'])-1:
+            print("},")
+        else:
+            print("}")
+        i += 1
+    print("]")
