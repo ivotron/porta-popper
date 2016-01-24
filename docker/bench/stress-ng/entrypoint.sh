415c980ef5dbd64bf4ffd56821f9d57d38d8042f
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-01-23 23:28:48 -0800

    stress-ng: adds more cpu-methods

--- docker/bench/stress-ng/entrypoint.sh
+++ docker/bench/stress-ng/entrypoint.sh
@@ -24,7 +24,7 @@ need_comma=false
 echo "["
 
 if [[ $BENCHMARKS == *"cpu-methods"* ]] ; then
-  for method in ackermann bitops callfunc crc16 cdouble cfloat clongdouble correlate decimal32 decimal64 decimal128 double djb2a euler explog fibonacci fnv1a fft float gamma gcd gray hamming hanoi hyperbolic idct int128 int64 int32 int16 int8 int128float int128double int128longdouble int128decimal32 int128decimal64 int128decimal128 int64float int64double int64longdouble int32float int32double int32longdouble jenkin jmp ln2 longdouble loop matrixprod nsqrt omega phi pi pjw prime psi rand rgb sdbm sieve sqrt trig zeta ; do
+  for method in ackermann bitops callfunc cdouble cfloat clongdouble correlate crc16 decimal32 decimal64 decimal128 dither djb2a double euler explog fft fibonacci float fnv1a gamma gcd gray hamming hanoi hyperbolic idct int128 int64 int32 int16 int8 int128float int128double int128longdouble int128decimal32 int128decimal64 int128decimal128 int64float int64double int64longdouble int32float int32double int32longdouble jenkin jmp ln2 longdouble loop matrixprod nsqrt omega parity phi pi pjw prime psi queens rand rand48 rgb sdbm sieve sqrt trig union zeta ; do
      stress-ng --cpu $NUM_WORKERS --cpu-method $method -t $TIMEOUT --metrics-brief --times -Y /out.yml &> /dev/null
      /postprocess.py cpu $method
      if [ "$method" != "zeta" ] ; then
