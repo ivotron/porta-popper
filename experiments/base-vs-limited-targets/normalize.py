7a13185872bc73719048a1bd8a76f68a06798e13
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-08-25 01:28:49 -0700

    Makes use of 'baseliner' role for base-vs-limited-targets experiment

--- experiments/base-vs-limited-targets/normalize.py
+++ experiments/base-vs-limited-targets/normalize.py
@@ -1,42 +1,33 @@
-#!/usr/bin/env python
-import sys
 import pandas as pd
 
-df = pd.read_csv('/data/alltests.csv')
 
-if len(sys.argv) != 2:
-    raise Exception("Expecting one argument (base system name)")
+def obtain_speedup(df, base_machine):
+   """Obtains a 'speedup' column w.r.t. a given base machine"""
+    # get a dataframe with base results; only columns 'benchmark' and 'result'
+    predicate = (df['machine'] == base_machine) & (df['limits'] == 'no_limits')
+    base_results = df[predicate][['benchmark', 'result']]
 
-base_machine = sys.argv[1]
+    # rename the 'result' column
+    base_results.rename(columns={'result': 'base_result'}, inplace=True)
 
-# get a dataframe base results only with columns 'benchmark' and 'result'
-predicate = (df['machine'] == base_machine) & (df['limits'] == 'without')
-base_results = df[predicate][['benchmark', 'result']]
+    # merge all tests with the base_results column (join on 'benchmark' column)
+    df = pd.merge(base_results, df)
 
-# rename the 'result' column
-base_results.rename(columns={'result': 'base_result'}, inplace=True)
+    # and exclude the base system itself
+    df = df[df['machine'] != base_machine]
 
-# merge all tests with the base_results column (i.e. join on 'benchmark' column)
-df = pd.merge(base_results, df)
+    # lastly, get normalized results for target system w.r.t. the base system
+    def get_speedup(row):
+        if row['lower_is_better'] is True:
+            speedup = row['base_result'] / row['result']
+        else:
+            speedup = row['result'] / row['base_result']
 
-# and exclude the base system itself
-df = df[df['machine'] != base_machine]
+        if speedup < 1.0:
+            speedup = 1 / speedup
 
-happened = False
+        return speedup
 
+    df['speedup'] = df.apply(get_speedup, axis=1)
 
-# lastly, get normalized results for target systems w.r.t. the base system
-def normalize(row):
-    if row['lower_is_better'] is True:
-        return row['base_result'] / row['result']
-    else:
-        global happened
-        happened = True
-        return row['result'] / row['base_result']
-
-df['normalized'] = df.apply(normalize, axis=1)
-
-print("happened: " + str(happened))
-
-# and rewrite the results, now including the normalized column
-df.to_csv('/data/alltests_with_normalized_results.csv', index=False)
+    return df
