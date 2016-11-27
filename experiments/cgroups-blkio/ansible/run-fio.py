4c1151553be33104c250e543f401a3194b44ea9b
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-11-26 19:05:10 -0800

    Modifies blkio experiment so it leverages baseliner
    Baseliner now supports iterations and parametrized testing. So instead
    of having ad-hoc code to deal with this, we make use of baseliner's
    features and minimize code.
    
    This version also executes the blkio experiment with respect to a device
    baseline max bandwidth performance (as opposed to using absolute KB
    limits).

--- experiments/cgroups-blkio/ansible/run-fio.py
+++ experiments/cgroups-blkio/ansible/run-fio.py
@@ -8,11 +8,12 @@ if len(sys.argv) != 8:
     print("""Usage:
   1 - maxbw list (comma-separated; one per mode: read,write,randread,randwrite)
   2 - modes list (comma-separated; one per container)
-  3 - limits list (comma-separated; one per container)
+  3 - limits list (comma-separated; one per container; in percentage of maxbw)
   4 - blksize
   5 - device
   6 - runtime
   7 - output_folder""")
+    raise Exception("Bad num of arguments")
 
 maxbw = sys.argv[1].split(',')
 modes = sys.argv[2].split(',')
@@ -25,11 +26,24 @@ output_folder = sys.argv[7]
 if len(maxbw) != 4:
     raise Exception("Expecting maxbw list of size 4")
 
+if len(modes) != len(limits):
+    raise Exception("Expecting as many modes as limits")
+
+if output_folder.strip() == "":
+    raise Exception("Output folder cannot be empty")
+
 sum = 0
 for limit in limits:
-    sum += limit
+    sum += int(limit)
 if sum > 100:
     raise Exception("Sum of all limits should be <= 100")
+
+if output_folder == "/":
+    raise Exception("Can't use '/' as output folder, we'll 'rm -rf /*' !!!")
+
+# delete previous results
+sh("sudo rm -fr {}/*".format(output_folder), shell=True)
+sh("mkdir -p " + output_folder, shell=True)
 
 sh(['docker', 'pull', 'ivotron/fio'])
 
@@ -39,47 +53,50 @@ sh(['docker', 'pull', 'ivotron/fio'])
 # only interested in measuring the performance of read/write
 # requests and not checking their integrity.
 # see: http://www.spinics.net/lists/fio/msg03295.html
-#
-# {
 flags = "--entrypoint=genfio-test"
 img = "ivotron/fio"
 args = "-s -d {} -r {} -b {}k".format(device, runtime, bs)
-# }
 
 # create containers first, to minimize startup costs
 cnames = []
 k = 1
-for mode, limit_percentage in zip(modes, limits):
+for mode, limit in zip(modes, limits):
 
+    limit_percentage = int(limit)
     if limit_percentage < 1 or limit_percentage > 100:
         raise Exception("Limit should be a percentage of max bandwidth")
 
-    # get bandwidth w.r.t. maxbw
+    # get max bandwidth for given mode
     if mode == "read":
-        limitkb = (limit_percentage / 100.0) * maxbw[0]
+        maxbw_for_mode = int(maxbw[0])
     elif mode == "write":
-        limitkb = (limit_percentage / 100.0) * maxbw[1]
+        maxbw_for_mode = int(maxbw[1])
     elif mode == "randread":
-        limitkb = (limit_percentage / 100.0) * maxbw[2]
+        maxbw_for_mode = int(maxbw[2])
     elif mode == "randwrite":
-        limitkb = (limit_percentage / 100.0) * maxbw[3]
+        maxbw_for_mode = int(maxbw[3])
     else:
         raise Exception("Unknown mode " + mode)
+
+    # get bandwidth limit w.r.t. maxbw
+    limitkb = (limit_percentage / 100.0) * maxbw_for_mode
 
     # round
     limitkb = int(limitkb)
 
-    results_folder = "{}/mode/{}/limit/{}".format(output_folder, mode, limitkb)
-    cname = "fio-" + k
+    results_folder = (
+        "{}/mode/{}/maxbw/{}/limit/{}"
+    ).format(output_folder, mode, maxbw_for_mode, limitkb)
+    cname = "fio-" + str(k)
     cmd = (
       "docker create"
       " {0} "
       " --name={1} "
       " --device {2}:{2} "
-      " --device-write-bps ${2}:${3}kb "
-      " --device-read-bps ${2}:${3}kb "
+      " --device-write-bps {2}:{3}kb "
+      " --device-read-bps {2}:{3}kb "
       " --volume {4}:/results "
-      " {5} {6} -m {8}"
+      " {5} {6} -m {7}"
     ).format(flags, cname, device, limitkb, results_folder, img, args, mode)
     sh(cmd, shell=True)
     cnames += [cname]
@@ -93,8 +110,9 @@ exit_codes = sh(["docker", "wait"] + cnames)
 sh(["docker", "rm"] + cnames)
 
 # check for errors
-if sh("echo {} | grep 0 | sed 's/0//g' | sed 's/ //g'", shell=True) != "":
-    print("Non-zero exit from one of the containers")
-    sys.exit(1)
+exit_codes = exit_codes.strip().splitlines()
+
+if True in [x is not "0" for x in exit_codes]:
+    raise Exception("Non-zero exit from one of the containers")
 
 sys.exit(0)
--- experiments/cgroups-blkio/run.sh
+++ experiments/cgroups-blkio/run.sh
@@ -1,47 +1,75 @@
 #!/bin/bash
 set -e -x
 
-NUM_REPETITIONS=5
+# this should be run from the experiments/cgroups-blkio folder
 
-# run baseliner to get IO baseline of the device
-docker run --rm -ti \
-  -v `pwd`/../results:/results \
-  -v `pwd`:/experiment \
-  -v "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" \
-  -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" \
-  --workdir=/experiment \
-  --net=host \
-  --entrypoint=/bin/bash \
-  williamyeh/ansible:debian8 -c \
-    "ssh-add -l &&
-     ansible-playbook
-       -e results_path=/results/baseline
-       -e @baseline-vars.yml
-       playbook.yml"
+# delete any possible file in all hosts
+#docker run --rm -ti \
+#  -v `pwd`/ansible:/experiment \
+#  -v "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" \
+#  -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" \
+#  --workdir=/experiment \
+#  --net=host \
+#  --entrypoint=/bin/bash \
+#  ivotron/ansible:2.2.0.0 -c \
+#    'ssh-add -l && \
+#     ansible all \
+#      -m shell \
+#      -a "sudo rm -fr /tmp/results/ && mkdir /tmp/results"'
+#sudo rm -fr results/baseline
+#
+## run baseliner to get IO baseline of the device
+#docker run --rm -ti \
+#  -v `pwd`/ansible:/experiment \
+#  -v `pwd`/../../.vendor:/experiment/vendor \
+#  -v `pwd`/results:/results \
+#  -v "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" \
+#  -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" \
+#  --workdir=/experiment \
+#  --net=host \
+#  --entrypoint=/bin/bash \
+#  ivotron/ansible:2.2.0.0 -c \
+#    'ssh-add -l && \
+#     ansible-playbook \
+#       -e results_path=/results/baseline \
+#       -e "@baseline-vars.yml" \
+#       playbook.yml'
 
 # obtain CSV for baseline results
 docker run --rm \
   -v `pwd`/results/baseline:/data \
   -v `pwd`/results/csv_spec.yml:/csv_spec.yml \
-  ivotron/json-to-table --spec /csv_spec.yml > results/baseline.csv
+  ivotron/json-to-table --csv-spec /csv_spec.yml > results/baseline.csv
 
 # extract a "read,write,randread,randwrite" max bandwidth comma-sep list
-maxbw=`foo`
+maxbw="`cat results/baseline.csv | grep ',"read' | awk -F',' '{print $4}'`"
+maxbw+=",`cat results/baseline.csv | grep ',"write' | awk -F',' '{print $5}'`"
+maxbw+=",`cat results/baseline.csv | grep ',"randread' | awk -F',' '{print $4}'`"
+maxbw+=",`cat results/baseline.csv | grep ',"randwrite' | awk -F',' '{print $5}'`"
+
+NUM_REPETITIONS=3
+
+cat ansible/vars.yml > ansible/vars_with_maxbw.yml
+echo "maxbw: \"${maxbw}\"" >> ansible/vars_with_maxbw.yml
+
+sudo rm -fr results/with_limits/
+mkdir -p results/with_limits/repetition
 
 # iterate
 for i in $(seq 1 $NUM_REPETITIONS); do
   docker run --rm -ti \
-    -v `pwd`/../results:/results \
-    -v `pwd`:/experiment \
+    -v `pwd`/ansible:/experiment \
+    -v `pwd`/../../.vendor:/experiment/vendor \
+    -v `pwd`/results:/results \
     -v "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" \
     -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" \
     --workdir=/experiment \
     --net=host \
     --entrypoint=/bin/bash \
-    williamyeh/ansible:debian8 -c \
-      "ssh-add -l &&
-       ansible-playbook
-         -e results_path=/results/with_limits/repetition/$i
-         -e maxbw=${maxbw}
-         -e @vars.yml playbook.yml"
+    ivotron/ansible:2.2.0.0 -c \
+      "ssh-add -l && \
+       ansible-playbook \
+         -e results_path=/results/with_limits/repetition/${i}/ \
+         -e @vars_with_maxbw.yml \
+         playbook.yml"
 done
