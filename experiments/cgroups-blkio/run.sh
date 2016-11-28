44452fcaabec94bff52bceae9f67eb4213504b7c
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-11-28 13:58:31 -0800

    Adds results for issdm-6

--- experiments/cgroups-blkio/run.sh
+++ experiments/cgroups-blkio/run.sh
@@ -4,36 +4,36 @@ set -e -x
 # this should be run from the experiments/cgroups-blkio folder
 
 # delete any possible file in all hosts
-#docker run --rm -ti \
-#  -v `pwd`/ansible:/experiment \
-#  -v "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" \
-#  -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" \
-#  --workdir=/experiment \
-#  --net=host \
-#  --entrypoint=/bin/bash \
-#  ivotron/ansible:2.2.0.0 -c \
-#    'ssh-add -l && \
-#     ansible all \
-#      -m shell \
-#      -a "sudo rm -fr /tmp/results/ && mkdir /tmp/results"'
-#sudo rm -fr results/baseline
-#
-## run baseliner to get IO baseline of the device
-#docker run --rm -ti \
-#  -v `pwd`/ansible:/experiment \
-#  -v `pwd`/../../.vendor:/experiment/vendor \
-#  -v `pwd`/results:/results \
-#  -v "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" \
-#  -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" \
-#  --workdir=/experiment \
-#  --net=host \
-#  --entrypoint=/bin/bash \
-#  ivotron/ansible:2.2.0.0 -c \
-#    'ssh-add -l && \
-#     ansible-playbook \
-#       -e results_path=/results/baseline \
-#       -e "@baseline-vars.yml" \
-#       playbook.yml'
+docker run --rm -ti \
+  -v `pwd`/ansible:/experiment \
+  -v "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" \
+  -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" \
+  --workdir=/experiment \
+  --net=host \
+  --entrypoint=/bin/bash \
+  ivotron/ansible:2.2.0.0 -c \
+    'ssh-add -l && \
+     ansible all \
+      -m shell \
+      -a "sudo rm -fr /tmp/results/ && mkdir /tmp/results"'
+sudo rm -fr results/baseline
+
+# run baseliner to get IO baseline of the device
+docker run --rm -ti \
+  -v `pwd`/ansible:/experiment \
+  -v `pwd`/../../.vendor:/experiment/vendor \
+  -v `pwd`/results:/results \
+  -v "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" \
+  -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" \
+  --workdir=/experiment \
+  --net=host \
+  --entrypoint=/bin/bash \
+  ivotron/ansible:2.2.0.0 -c \
+    'ssh-add -l && \
+     ansible-playbook \
+       -e results_path=/results/baseline \
+       -e "@baseline-vars.yml" \
+       playbook.yml'
 
 # obtain CSV for baseline results
 docker run --rm \
@@ -73,3 +73,9 @@ for i in $(seq 1 $NUM_REPETITIONS); do
          -e @vars_with_maxbw.yml \
          playbook.yml"
 done
+
+# obtain CSV for results
+docker run --rm \
+  -v `pwd`/results/with_limits:/data \
+  -v `pwd`/results/csv_spec.yml:/csv_spec.yml \
+  ivotron/json-to-table --csv-spec /csv_spec.yml > results/all.csv
