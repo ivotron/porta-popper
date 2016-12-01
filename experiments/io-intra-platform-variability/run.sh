8dbeb19067d84c7096ad35220c4df97f4067c762
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-12-01 10:30:53 -0800

    Minor tweaks to IO experiment

--- experiments/io-intra-platform-variability/run.sh
+++ experiments/io-intra-platform-variability/run.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 set -e -x
 
-NUM_REPETITIONS=10
-ANSIBLE_VARS=""
-DEVICE="/dev/sdb"
-
 # delete previous results
 sudo rm -fr results/output
 mkdir -p results/output
 
-# write device to vars file
+# get variables
+NUM_REPETITIONS=`cat ansible/vars.yml | grep num_repetitions | sed 's/num_repetitions://'`
+DEVICE=`cat ansible/vars.yml | grep device: | sed 's/device://'`
 echo "" > ansible/extra-vars.yml
-echo "  measure_runtime: yes" >> ansible/vars.yml
-echo "  devices:" >> ansible/vars.yml
-echo "  - $DEVICE:$DEVICE" >> ansible/vars.yml
 
 function run_baseliner() {
   ID=$1
   for i in $(seq 1 $NUM_REPETITIONS); do
+   mkdir -p results/output/$ID/repetition/${i}
    docker run --rm -ti \
      -v `pwd`/ansible:/experiment \
      -v `pwd`/../../.vendor:/experiment/vendor \
@@ -29,9 +25,13 @@ function run_baseliner() {
      --entrypoint=/bin/bash \
      ivotron/ansible:2.2.0.0 -c \
        "ssh-add -l && \
+        ansible all -m shell -a 'docker stop \$(docker ps -q); \
+                                 docker rm \$(docker ps -aq); \
+                                 true' && \
         ansible-playbook \
           -e results_path=/results/output/$ID/repetition/${i} \
-          -e @vars.yml -e extra-vars.yml \
+          -e @vars.yml \
+          -e @extra-vars.yml \
           playbook.yml"
   done
 
@@ -40,23 +40,21 @@ function run_baseliner() {
     -v `pwd`/results/output/$ID:/data \
     -v `pwd`/results/csv_spec.yml:/csv_spec.yml \
     ivotron/json-to-table --csv-spec /csv_spec.yml > results/$ID.csv
-
-  # obtain CSV of runtimes
-  echo "repetition,bench,runtime" > results/${ID}_runtimes.csv
-  for i in $(seq 1 $NUM_REPETITIONS); do
-    rt=`cat results/output/$ID/runtime`
-    echo "$i,$rt" >> results/${ID}_runtimes.csv
-  done
 }
 
 # run baseliner to get IO baseline of device
 run_baseliner "baseline"
 
 # get lowest common denominator
-LCD=`results/get_lcd results/baseline.csv`
+READ_LCD=`results/get_lcd results/baseline.csv read_iops 0.90`
+WRITE_LCD=`results/get_lcd results/baseline.csv write_iops 0.90`
+echo "READ_LCD: $READ_LCD"
+echo "WRITE_LCD: $WRITE_LCD"
 
 # run with limits
-echo "  limits:" >> ansible/vars.yml
-echo "  - $DEVICE:${LCD}kb" >> ansible/vars.yml
+echo "defaults:" >> ansible/extra-vars.yml
+echo "  limits:" >> ansible/extra-vars.yml
+echo "    device-read-iops: '{{ device }}:${READ_LCD}'" >> ansible/extra-vars.yml
+echo "    device-write-iops: '{{ device }}:${WRITE_LCD}'" >> ansible/extra-vars.yml
 
 run_baseliner "with_limits"
